6ee104d4a6e1323a070c33b7a7e52a514c0c8e41
WSudol <wsudol@pgs-soft.com>
2020-04-07 09:30:01 +0200

    Remove Server

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../shared/constants/in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false },
+    ),
   ],
   exports: [
     BrowserModule,
---
+++ src/app/shared/constants/heroes.mocks.ts
@@ -0,0 +1,14 @@
+import { IHero } from '../interfaces/hero.interface';
+
+export const HEROES: IHero[] = [
+    { id: '11', name: 'Dr Nice', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '12', name: 'Narco', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '13', name: 'Bombasto', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '14', name: 'Celeritas', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '15', name: 'Magneta', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '16', name: 'RubberMan', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '17', name: 'Dynama', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '18', name: 'Dr IQ', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '19', name: 'Magma', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+    { id: '20', name: 'Tornado', colorTheme: "", character: "", crimes: 0, dob: null, weapons:null },
+  ];
